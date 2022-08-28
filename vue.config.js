799b511d7c2c79f6252e12b9708886cd983d2bba
Victor Peixoto <peixoto2112@gmail.com>
2022-08-28 15:12:24 -0300

    mais ajustes

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
   ?'/MakeYourBurgerProjectVue/'
   :'/'
   // baseUrl: '/MakeYourBurgerProjectVue'
-})
+}
